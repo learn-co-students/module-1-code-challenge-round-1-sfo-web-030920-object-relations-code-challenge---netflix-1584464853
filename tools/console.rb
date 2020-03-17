0fe274988de4c0d3a0d557486c6b264a609acbd7
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-17 11:02:42 -0700

    Initial commit

--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,16 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+#
+#
+movie1 = Movie.new('Avenger 1')
+movie2 = Movie.new('Titanic')
+
+user1 = Viewer.new('Eduardo')
+user2 = Viewer.new('Alexia')
+
+review1 = Review.new(user1, movie1, 4.5)
+review2 = Review.new(user2, movie2m, 4.9)
 
 
 
