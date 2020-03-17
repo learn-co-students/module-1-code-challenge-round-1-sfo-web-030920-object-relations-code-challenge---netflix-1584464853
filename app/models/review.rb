89bc81c4edb2dbd8ca7939d7949611fd2755cba7
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-17 11:51:41 -0700

    Fixed commit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,24 @@
 class Review
+  attr_accessor :movie, :viewer, :rating
+  # attr_reader :rating
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(viewer, movie, rating)
+    @rating = rating
+    @viewer = viewer
+    @movie = movie
+    save
+  end
+
+  private
+
+  def save
+    @@all << self
+  end
 
 end
