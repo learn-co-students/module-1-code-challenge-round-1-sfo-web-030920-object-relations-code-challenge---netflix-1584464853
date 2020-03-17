f588b8bc19cb8d03102a97a73f22e8ea8fe5cbac
narduc <radnielpadron@gmail.com>
2020-03-17 11:06:23 -0700

    still working

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,39 @@
+
+
+
 class Review
 
+    attr_accessor :viewer, :movie, :rating
 
+    @@all = []
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def viewer(viewer)
+
+    end
+
+    def movie(movie)
+
+    end
 end
+
+#### Review
+
+# - `Review#viewer`
+#   - returns the `Viewer` instance associated with the
+#    `Review` instance
+# - `Review#movie`
+#   - returns the `Movie` instance associated with the 
+#   `Review` instance
+
+
+
